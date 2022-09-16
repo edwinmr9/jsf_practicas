3f48bc78f445a30a6eb468213a9d787d5cbf148a
LAPTOP\Host09 <edwmr9@gmail.com>
2022-09-17 01:42:13 +0200

    primer commit

---
+++ 01_jsf_eg_01/src/main/java/managed/LoginBean.java
@@ -0,0 +1,29 @@
+package managed;
+
+
+public class LoginBean {
+	private String usuario;
+	private String password;
+	public String getUsuario() {
+		return usuario;
+	}
+	
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPassword() {
+		return password;
+	} 
+	public void setPassword(String password) {
+		this.password = password;
+	} 
+	
+	public String login() { 
+		if(usuario.equals("curso") && password.equals("curso")) {
+			return "bienvenida";
+		}else {
+			return "error"; 
+		}
+		
+	}
+}
